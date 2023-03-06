c4081356606e5da781c89fea8d2e2fcb205576b9
Arindam Saha <arindamsaha1507@gmail.com>
2023-03-06 08:43:27 +0000

    Refreshes the references on each run

--- run.py
+++ run.py
@@ -1,13 +1,15 @@
-"""Tester"""
+"""Main module for analysis of a text"""
 
+import sys
 import kaavyamanjari.kaavyamanjari as km
 
+km.create_reference('sandarbha.yml', 'reference.csv')
 
-TEXT = 'champuuraamaayana.txt'
+TEXT = 'niitishataka.txt'
 
 anuchchhedas = km.create_anuchchheda_list(TEXT)
 
 with open('logger.txt', 'w', encoding='utf-8') as logger:
-    # sys.stdout = logger
+    sys.stdout = logger
     for anuchchheda in anuchchhedas:
         print(anuchchheda)
